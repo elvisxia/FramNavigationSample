b8bef6e19e06483c5de38472135b246d9dd5afc7
v-xucxia <v-xucxia@PC-ELVISXIA>
2016-08-01 14:38:51 +0800

    Add project files.

---
+++ FramNavigationSample/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using FramNavigationSample.Pages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FramNavigationSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+
+        private void btnPageOne_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(PageOne));
+        }
+
+        private void btnPageTwo_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(PageTwo));
+        }
+
+        private void btnPageThree_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(PageThree));
+        }
+    }
+}
---
+++ FramNavigationSample/Pages/PageOne.xaml.cs
@@ -0,0 +1,47 @@
+﻿using FramNavigationSample.Pages.SubPageForPageOne;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FramNavigationSample.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PageOne : Page
+    {
+        public PageOne()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+
+        private void myBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Frame newFrame = new Frame{
+                Width=100,
+                Height=100
+            };
+            myStackPanel.Children.Add(newFrame);
+            newFrame.Navigate(typeof(SubPageOne));
+                        
+        }
+    }
+}
